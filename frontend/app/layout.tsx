f3b6894aa6665780dc71ad00f4c7b9d645780179
Botij0 <javier.gonzalezsolda@edu.uah.es>
2023-10-30 22:21:29 +0100

    Ajustes Navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import {Navbar} from './components/Navbar'
+import NavbarF from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <Navbar/>
+        <NavbarF/>
         {children}
       </body>
     </html>

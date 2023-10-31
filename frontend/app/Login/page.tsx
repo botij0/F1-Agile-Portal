7a0e47f1a4c3a8c3bbae593eda2356be013f7211
Gonzalo Romero <gonzalo.romeroe@edu.uah.es>
2023-10-31 12:58:00 +0100

    creados formularios inicio sesión sin funcionalidad ni estilo

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,8 +1,51 @@
 export default function Login() {
-    return (
-        <div className='mt-[100px]'>
-            <h1 className='font-bold text-center uppercase text-3xl'>Iniciar Sesión</h1>
+  return (
+    <div className="mt-[100px]">
+      <form className="mx-4 max-w-xs">
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@example.com"
+            required
+          />
         </div>
-  
-    )
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-red-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-red-600 dark:ring-offset-red-800 dark:focus:ring-offset-red-800"
+              required
+            />
+          </div>
+          <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Recuérdame
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          Iniciar sesión
+        </button>
+      </form>
+    </div>
+  );
 }

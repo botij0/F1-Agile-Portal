9da1474b69f4382d0046361b8b91462a601a908a
mhem-space <mohamed.majjaoui@edu.uah.es>
2023-11-15 20:59:33 +0100

    Revisada

--- frontend/app/Noticias/page.tsx
+++ frontend/app/Noticias/page.tsx
@@ -44,7 +44,7 @@ export default function Noticias() {
           headers: {
             "Content-Type": "application/json",
             Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5OTkwNDM2NCwiZXhwIjoxNjk5OTkwNzY0fQ.mMo-NhaWYLqo8EqaPsgdV1TkNOSu654XTzrr_VKvHBM", //localStorage.getItem('token'),
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcwMDA3MzM2MSwiZXhwIjoxNzAwMTU5NzYxfQ.BpyEDIAeI18Sk8DwY3FOVaSG9RNMTU-jqEpGPyuJknI", //localStorage.getItem('token'),
           },
         });
         const data = await response.data;

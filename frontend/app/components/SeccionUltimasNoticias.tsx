dc81c693c8346c929f8a6230aac2f4cb661fbe85
mhem-space <mohamed.majjaoui@edu.uah.es>
2023-11-15 21:12:27 +0100

    Revisados errores carrusel

--- frontend/app/components/SeccionUltimasNoticias.tsx
+++ frontend/app/components/SeccionUltimasNoticias.tsx
@@ -48,8 +48,8 @@ export const SeccionUltimasNoticias = () => {
           type="button"
           data-te-target="#carouselExampleCaptions"
           data-te-slide-to="0"
-          data-te-carousel-active
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          data-te-carousel-active=""
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none !opacity-100"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
@@ -57,14 +57,14 @@ export const SeccionUltimasNoticias = () => {
           type="button"
           data-te-target="#carouselExampleCaptions"
           data-te-slide-to="1"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none !opacity-100"
           aria-label="Slide 2"
         ></button>
         <button
           type="button"
           data-te-target="#carouselExampleCaptions"
           data-te-slide-to="2"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none !opacity-100"
           aria-label="Slide 3"
         ></button>
       </div>
@@ -74,7 +74,7 @@ export const SeccionUltimasNoticias = () => {
         {/*First item*/}
         {/* <ImagenCarousel url={slides[0].url}/> */}
         <div
-          className="relative float-left -mr-[100%] w-full transition-transform duration-[400ms] ease-in-out motion-reduce:transition-none overflow-hidden"
+          className="relative float-left -mr-[100%] w-full transition-transform duration-[400ms] ease-in-out motion-reduce:transition-none overflow-hidden hidden !block data-[te-carousel-fade]:opacity-100 data-[te-carousel-fade]:z-[1]"
           data-te-carousel-active
           data-te-carousel-item
           style={{ backfaceVisibility: "hidden" }}

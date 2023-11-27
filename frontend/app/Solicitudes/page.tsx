704127e610535c3691afe86c9fb5c17b2355ed59
Gonzalo Romero <gonzalo.romeroe@edu.uah.es>
2023-11-27 11:15:17 +0100

    diseño tabla solicitudes sin funcionalidad

---
+++ frontend/app/Solicitudes/page.tsx
@@ -0,0 +1,9 @@
+import TablaSolicitudes from "@/app/components/TablaSolicitudes";
+
+export default function Solicitudes() {
+  return (
+    <div className="relative overflow-x-auto mt-[80px]">
+      <TablaSolicitudes />
+    </div>
+  );
+}

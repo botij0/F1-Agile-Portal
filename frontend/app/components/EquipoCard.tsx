27db1ce3629850a993bcc0cab6a1a315ae553f1a
Gonzalo Romero <gonzalo.romeroe@edu.uah.es>
2023-11-29 17:52:14 +0100

    página principal de equipos terminada, faltan las redirecciones

--- frontend/app/components/EquipoCard.tsx
+++ frontend/app/components/EquipoCard.tsx
@@ -17,17 +17,20 @@ const EquipoCard: React.FC<Props> = ({ equipo }) => {
   const imagenUrl = IMGS_BASE_URL + equipo.logo + ".png";
 
   return (
-    <div className="w-64 text-center rounded overflow-hidden shadow-lg">
-      <div className="flex items-center justify-center h-32">
-        <img
-          className="w-20 h-auto"
-          src={imagenUrl}
-          alt={equipo.nombre + " logo"}
-        />
-      </div>
-      <div className="px-6 py-4">
-        <p className="text-gray-700 text-base">{equipo.nombre}</p>
-      </div>
+    <div className="w-64 text-center rounded overflow-hidden shadow-lg hover:shadow-2xl">
+      {/** FALTA REDIRIGIR AL EQUIPO, PERO NO ESTABAN ACABADOS CUANDO LO IMPLEMENTÉ */}
+      <a href="http://localhost:3000">
+        <div className="flex items-center justify-center h-32 ">
+          <img
+            className="w-20 h-auto"
+            src={imagenUrl}
+            alt={equipo.nombre + " logo"}
+          />
+        </div>
+        <div className="px-6 py-4 bg-red-600">
+          <p className="text-white-700 text-xl">{equipo.nombre}</p>
+        </div>
+      </a>
     </div>
   );
 };

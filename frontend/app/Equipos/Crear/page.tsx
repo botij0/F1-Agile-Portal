7e616b731d343ddd283de82c22fff07d72a23ab2
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-22 20:03:16 +0100

    arreglo estético

--- frontend/app/Equipos/Crear/page.tsx
+++ frontend/app/Equipos/Crear/page.tsx
@@ -1,12 +1,11 @@
-import FormEquipo from "@/app/components/Equipos/FormEquipo"
+import FormEquipo from "@/app/components/Equipos/FormEquipo";
 
 const page = () => {
-  return (
-    
-      <div className="relative overflow-x-auto mt-[80px]">
-        <FormEquipo/>
-      </div>
-  )
-}
+    return (
+        <div className="relative overflow-x-auto">
+            <FormEquipo />
+        </div>
+    );
+};
 
-export default page
+export default page;

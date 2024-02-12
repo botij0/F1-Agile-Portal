ef82428ec0bd728197533f1173248890089647bb
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2024-02-12 13:55:49 +0100

    Arreglo conexión BBDD y sección de noticias

--- frontend/app/Noticias/Noticia/[id]/page.tsx
+++ frontend/app/Noticias/Noticia/[id]/page.tsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 import Constantes from "@/app/(utils)/constantes";
 import { getRequestTokenless } from "@/app/(utils)/api";
 import { useParams } from "next/navigation";
+import Loading from "@/app/components/Loading";
 
 export default function Noticias() {
     const [titular, setTitular] = useState("");
     const [cuerpo, setCuerpo] = useState("");
     const [imagen, setImagen] = useState("");
+    const [loading, setLoading] = useState(true);
 
     const params = useParams();
     const id = params.id;
@@ -17,13 +19,14 @@ export default function Noticias() {
         useEffect(() => {
             (async () => {
                 try {
+                    setLoading(true);
                     const response = await getRequestTokenless(
                         "noticias/" + id
                     );
-
                     setTitular(response.data.titulo);
                     setCuerpo(response.data.texto);
                     setImagen(response.data.imagen);
+                    setLoading(false);
                 } catch (error) {
                     console.log(error);
                 }
@@ -32,21 +35,30 @@ export default function Noticias() {
     }
 
     return (
-        <div className="mt-[20px] bg-red-600 rounded-2xl text-white w-[75%] mx-auto">
-            <div className="content-center mx-auto my-5 pt-2">
-                <h1 className="font-bold text-center text-4xl mx-5">
+        <div>
+            {loading ? (
+                <Loading />
+            ) : (
+        <div className="flex flex-col items-center justify-center">
+            <div className="content-center mx-auto border-b py-6 w-full shadow-sm ">
+                <h1 className="font-semibold text-center text-3xl mx-5">
                     {titular}
                 </h1>
             </div>
-            <div className="my-5">
-                <img
-                    className="mx-auto max-w-2xl"
-                    src={Constantes.IMAGE_BASE_URL + imagen}
-                ></img>
+            <div className="flex flex-col items-center w-[85%]">
+                <div className="my-5">
+                    <img
+                        className="w-[650px] rounded-t-lg shadow-lg border-4 border-red-600"
+                        src={Constantes.IMAGE_BASE_URL + imagen}
+                    ></img>
+                </div>
+                <div className="mx-auto w-[45%] pb-12 text-justify">
+                    <p dangerouslySetInnerHTML={{ __html: cuerpo }}></p>
+                </div>
             </div>
-            <div className="mx-auto w-[45%] pb-5">
-                <p className="text-left  ">{cuerpo}</p>
-            </div>
+        </div>
+    )}
+
         </div>
     );
 }

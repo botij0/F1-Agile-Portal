e1a2780e6f6044d3718ef0bbd660cf334e5039d6
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-02-13 19:08:39 +0100

    Mejorando vista piloto en detalle

--- frontend/app/components/Pilotos/DetallesPiloto.tsx
+++ frontend/app/components/Pilotos/DetallesPiloto.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Constantes from "@/app/(utils)/constantes";
 import { getRequestTokenless } from "@/app/(utils)/api";
 import Loading from "@/app/components/Loading";
+import Link from "next/link";
 
 interface Equipo {
     id: number;
@@ -47,84 +48,114 @@ function DetallesPiloto(params: { id: string }): React.JSX.Element {
 
             {!loading && piloto && (
                 <div className="m-4">
-                    <div className="mb-4">
-                        <p className="uppercase font-bold text-5xl">
+                    <div className="content-center mx-auto border-b py-6 w-full shadow-sm ">
+                        <h1 className="font-semibold text-center text-3xl mx-5">
                             {piloto.nombre + " " + piloto.apellidos}
-                        </p>
+                        </h1>
                     </div>
+                    <div className="flex flex-col gap-4  mt-5">
+                        <div className="w-[60%] flex items-center justify-center gap-16 mt-4 rounded-xl bg-gray-50 mx-auto py-10 shadow-xl flex-col xl:flex-row">
+                            <img
+                                src={Constantes.IMAGE_BASE_URL + piloto.foto}
+                                alt={"Cover"}
+                                className="w-auto rounded-lg max-h-64"
+                            />
+                            <div className="rounded-lg overflow-hidden border-red-500 border">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Nombre
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                {piloto.nombre}
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Apellidos
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                {piloto.apellidos}
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Equipo
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                <Link
+                                                    href={
+                                                        `/Equipos/Detalles/` +
+                                                        piloto.equipo.id
+                                                    }
+                                                    className="text-blue-500 hover:text-blue-700 underline"
+                                                >
+                                                    {piloto.equipo.nombre}
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Dorsal
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                {piloto.dorsal}
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Siglas
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                {piloto.siglas}
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                País
+                                            </td>
+                                            <td className="min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                <img
+                                                    src={`https://flagcdn.com/w20/${piloto.pais}.png`}
+                                                    width="35"
+                                                    alt={piloto.pais}
+                                                    className="border-2 border-black"
+                                                />
+                                            </td>
+                                        </tr>
+                                    </thead>
 
-                    <div className="flex flex-wrap">
-                        <div className="w-full md:w-1/2 lg:w-1/3 mt-4 md:mt-0">
-                            <div className="text-sm text-gray-500  mx-4 ">
-                                <img
-                                    src={
-                                        Constantes.IMAGE_BASE_URL + piloto.foto
-                                    }
-                                />
+                                    <thead className="border-red-500 border-t">
+                                        <tr>
+                                            <td className="w-fit p-2 font-bold">
+                                                Twitter
+                                            </td>
+                                            <td className="  min-w-60 w-60 border-l pl-2  border-red-500 pr-2">
+                                                <Link
+                                                    href={
+                                                        "https://twitter.com/" +
+                                                        piloto.twitter
+                                                    }
+                                                    className="text-blue-500 underline hover:text-blue-700"
+                                                >
+                                                    {"@" + piloto.twitter}
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                </table>
                             </div>
-                        </div>
-
-                        <div className="w-full md:w-1/2 lg:w-2/3 pr-4">
-                            <table className="text-left text-3xl">
-                                <tbody>
-                                    <tr>
-                                        <th>Nombre: </th>
-                                        <td>{piloto.nombre}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Apellidos: </th>
-                                        <td>{piloto.apellidos}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Equipo: </th>
-                                        <td>
-                                            <a
-                                                href={
-                                                    `/Equipos/Detalles/` +
-                                                    piloto.equipo.id
-                                                }
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="text-blue-500"
-                                            >
-                                                {piloto.equipo.nombre}
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Dorsal: </th>
-                                        <td>{piloto.dorsal}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Siglas: </th>
-                                        <td>{piloto.siglas}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>País: </th>
-                                        <td>
-                                            <img
-                                                src={`https://flagcdn.com/w20/${piloto.pais}.png`}
-                                                width="35"
-                                                alt={piloto.pais}
-                                                className="border-2 border-black"
-                                            />
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>Twitter: </th>
-                                        <td>
-                                            <a
-                                                href={`https://twitter.com/${piloto.twitter}`}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="text-blue-500"
-                                            >
-                                                @{piloto.twitter}
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
